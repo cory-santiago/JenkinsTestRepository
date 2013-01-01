6f66f0c4d159c0d8d6e2e53cf3ba382600beb7b8
Cory Santiago <cms5822@rit.edu>
2013-01-01 13:14:30 -0500

    Java Prime Number Calculator

---
+++ F.java
@@ -0,0 +1,11 @@
+public class F {
+   public static void main(String[] a) {
+      for (int i, j, k = 2; k-- > 0;)
+         for (i = 1; i++ < new Long(a[0]);) {
+            for (j = i; i % --j > 0;)
+               ;
+            if (k > 0 ? j < 2 : j > 1)
+               System.out.println(i);
+      }
+   }
+}
